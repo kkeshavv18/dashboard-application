7781a98dabf2d4f01b65a39b83028af5ffcf392f
keshav <keshav.kattel@waterflow.technology>
2025-12-03 20:36:21 +0545

    feat: setup react-router-dom for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import { Button } from "@/components/ui/button";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Data from "./pages/Data";
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+    <div className="flex min-h-svh flex-col items-center justify-center gap-4">
+      <nav className="flex gap-4">
+        <Link to="/">Home</Link>
+        <Link to="/data">Data</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/data" element={<Data />} />
+      </Routes>
     </div>
   );
 }
