13aaffa69894e2edbc56c5bef09722346a3982a0
keshav <keshav.kattel@waterflow.technology>
2025-12-03 19:40:54 +0545

    feat: initial project setup with vite and tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-center mt-1 text-green-400">
+        Hello World
+      </h1>
+    </>
+  );
+}
+
+export default App;
